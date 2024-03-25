84d94f150dbf8f416cb7791254d5fce16e14ffb1
Jeffy Yu <105130385+yu-jeffy@users.noreply.github.com>
2024-03-25 14:47:30 +0000

    about ideas

--- pages/about.js
+++ pages/about.js
@@ -10,6 +10,7 @@ const About = () => {
             <div className={styles.textContainer}>
                 <p>This is the about page</p>
                 <p>More information about us...</p>
+                <p>our platform helps the unbanked, disaster zones, anonymous donations, war relief efforts, and other populations, in addition to general fundraising for anyone</p>
             </div>
             <div className={styles.imageContainer}>
                 <Image src="/path/to/image1.jpg" alt="Image 1" width={500} height={300} />
